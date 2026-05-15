d8298877be876a92d7e17813a62ba5e4b7bc6ea9
KorpenM <magnuskorp@gmail.com>
2026-05-15 14:03:19 +0200

    Lägger till CodeQL och Trivy i CI-pipeline

--- src/Forged/obj/Debug/net10.0/Forged.AssemblyInfo.cs
+++ src/Forged/obj/Debug/net10.0/Forged.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Forged")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+712eb855e64881bfcfe03124bc4adc3a3108de4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47610908d08ce3bd64ae9ed9819877ea0eb38ec9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Forged")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Forged")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
